e11789fdbc7ea79f04439cd4e6a1360ad3a801b8
Nick Tran <37254566+tran-nick@users.noreply.github.com>
2020-04-25 14:02:03 -0700

    Add files via upload

---
+++ Section17/I_Printable.cpp
@@ -0,0 +1,6 @@
+#include "I_Printable.h"
+
+std::ostream& operator<<(std::ostream& out, const I_Printable& obj) {
+	obj.print(out);
+	return out;
+}
---
+++ Section17/I_Printable.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+class I_Printable
+{
+	friend std::ostream& operator<<(std::ostream& out, const I_Printable& obj);
+
+public:
+	virtual void print(std::ostream& out) const = 0;
+	virtual ~I_Printable() = default;
+
+};
+
